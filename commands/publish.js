66951a5fa0a5c3e62c7fbb40618ad480d78988b4
Vivek Agate <vivekagate@gmail.com>
2022-11-30 15:29:11 -0800

    Updates

--- commands/publish.js
+++ commands/publish.js
@@ -1,6 +1,33 @@
 import { executeRest } from '../rest-client.js';
+import * as fs from 'fs';
+import * as path from 'path';
 
-export async function publish(port) {
-  console.log('Connecting to cloud');
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
+
+function readFiles(buildDir) {
+  return getAllFiles(buildDir);
+}
+
+export async function publish(buildDir) {
+  console.log('Publish to Saasbit Cloud');
+  const files = readFiles(buildDir);
+  files.forEach((file) => {
+    console.log(`Publish file: ${file}`);
+  })
 }
 
--- index.js
+++ index.js
@@ -2,17 +2,46 @@
 import { program }  from 'commander';
 import { publish } from './commands/publish.js';
 import { getconfig, setconfig, validate } from './commands/config.js';
+import {readline} from 'readline';
 
-program.command('publish')
-  .description('Publish code to Saasbit Cloud')
-  .option('-b, --build <string>', 'Build directory', 'dist')
+const initializeConfigIfNeeded = async () => {
+  const cfg = getconfig();
+  if (!cfg.token || !cfg.app) {
+    console.error('Configure API Token and App name');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    const token = await rl.question('Enter the API Token: ');
+    const app = await rl.question('Enter the App Name: ');
+
+    setconfig(token, app, '');
+  }
+}
+program.command('publish-fe')
+  .description('Publish UI/Frontend code to Saasbit Cloud')
+  .option('-b, --build <BUILD DIRECTORY>', 'Build directory', 'dist')
+  .action(async (str, options) => {
+    try{
+      await initializeConfigIfNeeded();
+      console.log(str.build);
+      await publish(str.build);
+    }catch(e) {}
+  });
+
+program.command('publish-be')
+  .description('Publish Server/Backend code to Saasbit Cloud')
+  .option('-b, --build <BUILD DIRECTORY>', 'Build directory', 'dist')
+  .option('-f, --file <BUILD FILE>', 'Build File / Jar file', '')
   .action(async (str, options) => {
     console.log(str.build);
+    await initializeConfigIfNeeded();
     await publish(str.build);
   });
 
 program.command('config')
-  .description('Authenticate and configure')
+  .description('Authenticate and configure Saasbit CLI')
   .option('-t, --token <API TOKEN>', 'API Token')
   .option('-a, --app <APP NAME>', 'App Name')
   .action(async (str, options) => {
@@ -25,7 +54,7 @@ program.command('config')
         console.log('Missing TOKEN or App Name');
       }
     }else{
-      const cfg = getconfig(str.token,str.app);
+      const cfg = getconfig();
       console.log(JSON.stringify(cfg, null, 4));
     }
   });
